52924dbb2ace99ffae18c8b8a84a15d1be7f1f40
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2026-04-15 17:40:37 +0300

    fix: subpackage

--- mison/miner/__init__.py
+++ mison/miner/__init__.py
@@ -1,5 +1,5 @@
 from .miner import *
 
-__all__ = ['git_mine_commits', 'pydriller_mine_commits', 'github_mine_commits', 'Commit', 'ModifiedFile',
-           'CommitJSONEncoder', 'CommitJSONDecoder']
+__all__ = ['Commit', 'ModifiedFile', 'CommitJSONEncoder', 'CommitJSONDecoder',
+           'CommitMiner', 'GitMiner', 'PydrillerMiner', 'GithubMiner']
 
